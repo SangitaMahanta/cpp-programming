5d215cabaf11e4dfb767592772c2d23c6b2dd792
SangitaMahanta <sangitamiki1998@gmail.com>
2021-05-17 17:39:39 +0530

    recursionMethod

--- inlineFunction/inlinefunDefultAndConstantArgument.cpp
+++ inlineFunction/inlinefunDefultAndConstantArgument.cpp
@@ -11,15 +11,20 @@ inline int product(int a, int b){
 float moneyReceived(int currentMoney, float factor=1.04){
     return currentMoney * factor;
 }
+//default value most be assigned from right to left.
 
-// int strlen(const char *p){
 
-// }
+
+int strlen(const char p){
+return p+2;
+}
 int main(){
     int a, b;
+    int g=7;
     // cout<<"Enter the value of a and b"<<endl;
     // cin>>a>>b;
     // cout<<"The product of a and b is "<<product(a,b)<<endl;
+    cout<<strlen(9)<<endl;
     int money = 100000;
     cout<<"If you have "<<money<<" Rs in your bank account, you will recive "<<moneyReceived(money)<< "Rs after 1 year"<<endl;
     cout<<"For VIP: If you have "<<money<<" Rs in your bank account, you will recive "<<moneyReceived(money, 1.1)<< " Rs after 1 year";
